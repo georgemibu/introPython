5878a3955de4037014e665af35ae3da93fba841b
georgemibu <georgemibu@gmail.com>
2025-04-29 21:28:31 -0300

    Prueba de inventarios en array

--- ejercicios1.py
+++ ejercicios1.py
@@ -9,3 +9,15 @@ with open('clientes.txt', 'r') as archivo:
     for linea in archivo:
         print(linea.strip()) #strip quita el salto de línea
 
+#registro de movimientos contables en un archivo
+movimientos = [
+    "01/04/2025 - Venta - $5000",
+    "02/04/2025 - Pago servicios - $15000",
+    "03/04/2025 - Compra insumos - $10000",
+]
+
+with open('movimientos.txt', 'a') as archivo:
+    for movimiento in movimientos:
+        archivo.write(movimiento + '\n')
+
+
